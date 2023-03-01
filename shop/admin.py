658420a7e0736390ba6bda76bd66ae5414598b8a
cwill83247 <breakthrumusic@hotmail.com>
2023-03-01 11:40:15 +0000

    Add: Adding prodcuts and categories to the DJANGO admin management

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_code',
+        'name',
+        'category',
+        'price',
+        'customer_rating',
+        'image',
+    )
+
+    ordering = ('product_code',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+         'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
