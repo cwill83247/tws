7dd011b16314a46ce477b890746b8a0d0b9ba99d
cwill83247 <breakthrumusic@hotmail.com>
2023-03-08 13:12:17 +0000

    Add: Adding functionality to add items ot shopping bag, adding csrf token reference and Javascript

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import *
+from django.http import JsonResponse
+import json
+
 
 # Create your views here.
+
+
+# Function for when item is added to shopping bag
+def updateItem(request):
+    data = json.loads(request.data)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('Prpdcut Id:', productId)
+
+    return JsonResponse('Item add to bag', safe=False)
--- static/js/shoppingbag.js
+++ static/js/shoppingbag.js
@@ -10,5 +10,38 @@ for (var i= 0; i < addEventHandlertoBtns.length; i++){
        var productId =this.dataset.product
        var action =this.dataset.action
        console.log('productId:', productId, 'action:', action) 
+
+//check if user is logged in        
+       console.log('User:', user)
+       if (user == 'AnonymousUser'){
+        console.log('User not logged in')
+       }
+       else{
+         updateUserOrder(productId, action)
+       }
     })
-}    
+}  
+
+function updateUserOrder(productId, action){
+    console.log('user logged in')
+
+    var url ='/shoppingbag/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        body:JSON.stringify({'productId': productId, 'action': action})        
+    })
+    //promise being returned after function has been successful
+    //value from urls.py which points to views.py and update_item function, then converting to JSON 1st
+    .then((response) =>{
+        return response.json()
+    })
+    //grabbing above now converted to JSON and console.logging value from urls.py which points to views.py and update_item function
+   .then((data) =>{
+        console.log('data:',data)
+    })
+}
