fae02291488c308486d2faa83cd1f5ff98406fc5
cwill83247 <breakthrumusic@hotmail.com>
2023-03-09 16:13:18 +0000

    Update: updating so able to add items to the session cookie

--- shoppingbag/shoppingbag.py
+++ shoppingbag/shoppingbag.py
@@ -1,10 +1,11 @@
 from decimal import Decimal
 from django.conf import settings
+from decimal import Decimal
 from shop.models import Product
 from django.shortcuts import get_object_or_404
 
 
-Class Shopping_Bag:
+class Shopping_Bag:
     def __init__(self, request):
         """
         Initialize the shopping bag.
@@ -14,4 +15,57 @@ Class Shopping_Bag:
         if not twsshoppingbag:
             # if no shopping bag create an empty one 
             twsshoppingbag = self.session[settings.SHOPPINGBAG_SESSION_ID] = {}
-        self.cart = twsshoppingbag
+        self.bag = twsshoppingbag
+
+    # iterate over items in bag and get them form the DB
+    def __iter__(self):
+        
+        product_ids = self.bag.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        bag = self.bag.copy()
+        for product in products:
+            bag[str(product.id)]['product'] = product
+        for item in bag.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+
+    #adding product to the shopping bag checking if an item with same
+    #product id is present 
+    def add(self, product, quantity=1, override_quantity=False):
+
+        product_id = str(product_id)
+        if product_id not in self.cart:
+            self.bag[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        #if true prpduct/item is int he bag so increase the quantity                            
+        if override_quantity:
+            self.bag[product_id]['quantity'] = quantity
+        else:
+            self.bag[product_id]['quantity'] += quantity
+            self.save()
+    
+    #remove product/item form the shopping bag
+    def remove(self, product):
+       
+        product_id = str(product.id)
+        if product_id in self.bag:
+            del self.cart[product_id]
+            self.save()
+
+     # removes session for shopping bag do i need this ?????
+    def clear(self):
+        del self.session[settings.SHOPPINGBAG_SESSION_ID]
+        self.save()
+
+    #update total 
+    def get_order_total(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.bag.values())
+
+
+    # setting boolean to make sure we save session data
+    #changing session.modified to True DJANGO will know to save  
+    def save(self):        
+        self.session.modified = True        
+
--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from .models import *
 from django.http import JsonResponse
 import json
@@ -22,7 +22,15 @@ def updateItem(request):
 
     customer = request.user.customer    
     product = Product.objects.get(id=productId)
-    order, created = Order.objects.get_or_create(
-        customer=customer, complete=False)
+    #order, created = Order.objects.get_or_create(
+        #customer=customer, complete=False)
 
+    bag = request.session.get('bag', {})             # this is key as creates in  the http session and stores it until user closes browser   
+
+    if productId in list(bag.keys()):
+        bag[productId] += quantity
+    else:
+        bag[productId]  = quantity
+    
+    request.session['bag'] = bag
     return JsonResponse('Item add to bag', safe=False)
