e48fafa1038331c895189fd9b34f48930e1725cc
cwill83247 <breakthrumusic@hotmail.com>
2023-03-18 12:59:21 +0000

    Fix: Orders weren't pulling through into Django admin

--- shop/models.py
+++ shop/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     product_code = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    #has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     customer_rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     item_in_stock = models.BooleanField(default=True)                                                          # or could change this to number of items in stock ? 
