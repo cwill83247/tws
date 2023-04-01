764cb2b38adc0a7ada9fc2aa5e21fa984855634f
cwill83247 <breakthrumusic@hotmail.com>
2023-04-01 10:29:22 +0000

    Add: Add basic outline for Product admin form

---
+++ shop/forms.py
@@ -0,0 +1,14 @@
+from django import ofrms
+from .models import Product, Category
+
+class ProductForm (forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__ (self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories - Category.objects.all  
+        
+          
