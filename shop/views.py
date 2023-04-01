579084af4ba0b7a60e2c563268e0ba3a058d5bc6
cwill83247 <breakthrumusic@hotmail.com>
2023-04-01 11:31:53 +0000

    Add: Adding so only a super user can add products

--- shop/views.py
+++ shop/views.py
@@ -35,6 +35,10 @@ def product_detail(request, id):
 @login_required
 def add_product(request):
     
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only Adminstrators can do this.')
+        return redirect(reverse('home'))
+        
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
