80d6fccafe7cd06bcd3e4b7ed1b527fc4dd08b7e
cwill83247 <breakthrumusic@hotmail.com>
2023-04-10 10:48:58 +0000

    Add: Adding heroku app to csrf in settings

--- twsshop/settings.py
+++ twsshop/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['tws.herokuapp.com', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ["https://8000-cwill83247-tws-7trz5n9eoma.ws-eu93.gitpod.io", "https://www.8000-cwill83247-tws-t9qu1ypeu3s.ws-eu88.gitpod.io", "https://8000-cwill83247-tws-t9qu1ypeu3s.ws-eu89b.gitpod.io"]
+CSRF_TRUSTED_ORIGINS = ["https://tws.herokuapp.com","https://8000-cwill83247-tws-7trz5n9eoma.ws-eu93.gitpod.io", "https://www.8000-cwill83247-tws-t9qu1ypeu3s.ws-eu88.gitpod.io", "https://8000-cwill83247-tws-t9qu1ypeu3s.ws-eu89b.gitpod.io"]
 
 # Application definition
 
