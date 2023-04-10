8689e9cef0bd5fe4fb1a8f4dfb6d30708a7cd892
cwill83247 <breakthrumusic@hotmail.com>
2023-04-10 12:57:28 +0000

    Add: Adding if statement to settings so that Amazon Web Servcies is used for static files

--- twsshop/settings.py
+++ twsshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v^ynslhzfidy@acca6vj0d&%suk__2+-o(gjbgw09xfd*@lz0@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'discountcodes',
     'contactus',
+    'storages',
 
 ]
 
